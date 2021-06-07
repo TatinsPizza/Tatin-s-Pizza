df38231a4487b1979e8814ac760f30bb4b702641
Ricardo Millanao Burgos <r.millanao02@ufromail.cl>
2021-06-07 11:10:19 -0400

    agregando comentarios finales

--- app/views.py
+++ app/views.py
@@ -203,109 +203,136 @@ def comentario(request):
 
 # --Comida--
 def monitoreo_comidas(request):
-
+    #Obtenemos todas las comidas desde la base de datos
     comidas = Comida.objects.all()
-
+    #A traves del contexto enviaremos los datos
     contexto = {
         "comidas": comidas,
     }
-
+    #Finalmente renderizaremos la template y enviaremos el contexto
     return render(request, "monitoreo_comidas.html", contexto)
 
 def eliminar_comida(request,id):
-
+    #Obtenemos todas la comida coincidente seguna la variable id del parametro
     comida = Comida.objects.get(id_comida = id)
+    #Una vez obtenida la comida se procera a borrar
     comida.delete()
-
+    #Una vez borrada redirigiremos hacia la siguiente direccion
     return redirect("/tatinspizza.com/monitoreo_comidas")
 
 def crear_comida(request):
+    # Todo lo que se encuentra en el apartado del "if" es lo que se hara
+    # cuando se llegue a este metodo mediante el formulario
     if request.method == "POST":
+         # guardamos todas las variables que venian en el formulario en variables propias
+        # (el nombre de las variables que esta entre conchetes es el "name" que se les asigno en los input de la template )
         nombre = request.POST["nombre"]
         descripcion = request.POST["descripcion"]
         precio = request.POST["precio"]
 
+        #En caso que se repita el nombre se rederigira a la misma direccion para volver a repetir el proceso
         if Comida.objects.filter(nombre=nombre).exists():
             return redirect("/tatinspizza.com/crear_comida")
 
+        # si el metodo llego hasta aqui es porque se cumplieron las condiciones
+        # asi que creamos una intancia de Comida y le asignamos sus respectivos atributos
         nueva_comida = Comida()
         nueva_comida.nombre = nombre
         nueva_comida.descripcion = descripcion
         nueva_comida.precio = precio
+        #luego de asignados los datos lo guardaremos
         nueva_comida.save()
 
+        #de completado el proceso se redirigira a la siguiente direccion
         return redirect("/tatinspizza.com/monitoreo_comidas")
-
+    #en primera instancia se renderizara hacia la template
     return render(request,"crear_comida.html")
 
 def editar_comida(request, id):
+    # Todo lo que se encuentra en el apartado del "if" es lo que se hara
+    # cuando se llegue a este metodo mediante el formulario
     if request.method == "POST":
+         # guardamos todas las variables que venian en el formulario en variables propias
+        # (el nombre de las variables que esta entre conchetes es el "name" que se les asigno en los input de la template )
         nombre = request.POST["nombre"]
         descripcion = request.POST["descripcion"]
         precio = request.POST["precio"]
-
+        
+        # si el metodo llego hasta aqui es porque se cumplieron las condiciones
+        # asi que creamos una intancia de Comida y le asignamos sus respectivos atributos
         actualizar_comida = Comida.objects.get(id_comida=id)
         actualizar_comida.nombre = nombre
         actualizar_comida.descripcion = descripcion
         actualizar_comida.precio = precio
+         #luego de asignados los datos lo guardaremos
         actualizar_comida.save()
 
-        
+        #de completado el proceso se redirigira a la siguiente direccion
         return redirect("/tatinspizza.com/monitoreo_comidas")
-
+    #Obtenemos los datos de la comida coincidente segun el id como parametro
     comida = Comida.objects.get(id_comida=id)
-
+    #Asignaremos la comida coincidente a un diccionario para enviar los datos hacia la template
     contexto = {
         "comida": comida,
     }
-
+    #en primera instancia se renderizara hacia la template
     return render(request, "editar_comida.html", contexto)
 
 # --Cliente--
 
 
 def monitoreo_usuarios(request):
-
+    #Obtenermos todas los usuarios de la Base de datos
     usuarios = Usuario.objects.all()
-
+    #Atraves de este contexto enviaremos la informacion obtenida de los usuarios
     contexto = {
         "usuarios": usuarios,
     }
-
+    #Finalmente renderizaremos la template enviandole el contexto
     return render(request, "monitoreo_usuarios.html", contexto)
 
 def eliminar_usuario(request,id):
-   
+   #Obtenermos todas los usuarios de la Base de datos
     usuario = Usuario.objects.get(id_usuario = id)
+    #Ya obtenido el usuario lo borraremos
     usuario.delete()
-
+    #Luego de borrado rederigiremos hace la template
     return redirect("/tatinspizza.com/monitoreo_usuarios")
 
 def editar_usuario(request, id):
+    # Todo lo que se encuentra en el apartado del "if" es lo que se hara
+    # cuando se llegue a este metodo mediante el formulario
     if request.method == "POST":
+         # guardamos todas las variables que venian en el formulario en variables propias
+        # (el nombre de las variables que esta entre conchetes es el "name" que se les asigno en los input de la template )
         nombre = request.POST["nombre"]
         correo = request.POST["correo"]
         contrasena2 = request.POST["contrasena1"]
         contrasena1 = request.POST["contrasena2"]
 
-        
+        #si es que la contraseñas del formulario no son coincidentes se rederigirá de nuevo a la misma direccion sin permitir la edicion del usuario
         if contrasena1 != contrasena2:
             return redirect("/tatinspizza.com/editar_usuario/"+str(id))
 
+         # si el metodo llego hasta aqui es porque se cumplieron las condiciones
+        # asi que creamos una intancia de Comida y le asignamos sus respectivos atributos
         actualizar_usuario = Usuario.objects.get(id_usuario=id)
         actualizar_usuario.nombre = nombre
         actualizar_usuario.correo = correo
         actualizar_usuario.contrasena = contrasena1
+        #luego guardaremos 
         actualizar_usuario.save()
 
+        #Luego de editado el usuario se redirigira hacia la siguiente ruta
         return redirect("/tatinspizza.com/monitoreo_usuarios")
 
+    #Obtenemos el usuario coincidente con el id del parametro
     usuario = Usuario.objects.get(id_usuario=id)
-
+    #Asignaremos el usuario coincidente a un diccionario para enviar los datos hacia la template
     contexto = {
         "usuario": usuario,
     }
-
+    #en primera instancia se renderizara hacia la template
     return render(request, "editar_usuario.html", contexto)
 
 def boleta(request):
